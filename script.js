5db1545290194e0c3a0ebbd56ef48d118fb07085
Ariana <arianaholverson@gmail.com>
2019-02-22 20:19:38 -0800

    Done, popup does NOT work
    Javascript error line 6 and html 37

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPopup () {
+	var popup = document.getElementById('popup');
+	popup.classlist.add('show')
+}
+
+function hidePopup () {
+	var popup = document.getElementById('popup');
+	popup.classlist.remove('show')
+}
